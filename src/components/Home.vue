737839f34edda0548b1c4541d0846b3b18b9ffeb
Jana Marie Bantolino <102508350+anaj00@users.noreply.github.com>
2024-04-06 01:23:14 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <body>
+    <v-card class="w-75 h-75 pa-4">
+
+      <template v-slot:text>
+        <div class="d-flex justify-space-between align-center">
+
+          <div class="w-100 mr-6">
+            <v-text-field v-model="name" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
+              single-line>
+            </v-text-field>
+          </div>
+
+          <v-btn class="text-white font-weight-bold text-capitalize mr-3" color="var(--vt-c-primary)" height="50">
+            Generate Report
+          </v-btn>
+
+          <v-btn class="text-white font-weight-bold text-capitalize" color="var(--vt-c-primary)" height="50">
+            Add record
+          </v-btn>
+
+        </div>
+      </template>
+
+      <v-data-table-server v-model:items-per-page="itemsPerPage" :headers="headers" :items="serverItems"
+        :items-length="totalItems" :loading="loading" :search="search" item-value="name" @update:options="loadItems">
+
+
+
+      </v-data-table-server>
+
+    </v-card>
+
+  </body>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Import stylesheets
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import './assets/main.css'
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
